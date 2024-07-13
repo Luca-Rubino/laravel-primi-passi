5743bf752f9070f7de699c4d40069e9b60c64a6e
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-13 11:38:48 +0200

    modificare indirizzi in href

--- resources/views/about-me.blade.php
+++ resources/views/about-me.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.scss">
     <title>Laravel_Primi_Passi</title>
 </head>
 <body>
@@ -21,7 +20,7 @@
         </ul>
     </nav>
     <header>
-        <h1>Hello World</h1>
+        <h1>About Me</h1>
     </header>
 </body>
 </html>
